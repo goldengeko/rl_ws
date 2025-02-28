de8c61a1736774af75de9f52244d3e4e6a6fefd7
Ninad Kulkarni <ninad.kulkarni@alumni.fh-aachen.de>
2025-02-28 16:22:54 +0100

    modified, new file

--- src/webots_ros2_spot/launch/spot_launch.py
+++ src/webots_ros2_spot/launch/spot_launch.py
@@ -3,6 +3,8 @@
 import os
 import launch
 from launch import LaunchDescription
+from launch.actions import EmitEvent
+from launch.events import Shutdown
 from launch.substitutions.path_join_substitution import PathJoinSubstitution
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
@@ -16,7 +18,49 @@ from webots_ros2_driver.wait_for_controller_connection import (
 package_dir = get_package_share_directory("webots_spot")
 
 
-# Define all the ROS 2 nodes that need to be restart on simulation reset here
+def create_slam_and_octomap_nodes():
+    slam_toolbox = Node(
+        parameters=[
+            {"use_sim_time": True},
+            {"base_frame": "base_link"},
+            {"max_laser_range": 10.0},
+        ],
+        package="slam_toolbox",
+        executable="async_slam_toolbox_node",
+        name="slam_toolbox",
+        output="screen",
+    )
+    slam_rviz_config = os.path.join(
+        get_package_share_directory("webots_spot"), "resource", "rl.rviz"
+    )
+    slam_rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=["--display-config=" + slam_rviz_config],
+        parameters=[{"use_sim_time": True}],
+    )
+    octomap_server = Node(
+        package='octomap_server',
+        executable='octomap_server_node',
+        output='screen',
+        parameters=[{
+            "resolution": 0.1,
+            "frame_id": "map",
+            "base_frame_id": "base_link",
+            "latch": False,
+            "exploration": True,
+            "multiple_pointclouds": False,
+            "use_sim_time": True,
+        }],
+        remappings=[
+            ("cloud_in", "/Spot/Velodyne_Puck/point_cloud")
+        ],
+    )
+    return [slam_toolbox, slam_rviz, octomap_server]
+
+
+# Define all the ROS 2 nodes that need to be restarted on simulation reset here
 def get_ros2_nodes(*args):
     # SpotArm Driver node
     spotarm_ros2_control_params = os.path.join(
@@ -85,14 +129,21 @@ def get_ros2_nodes(*args):
         output="screen",
     )
 
-    return [spotarm_driver, waiting_nodes, initial_manipulator_positioning]
+    slam_and_octomap_nodes = create_slam_and_octomap_nodes()
+
+    return [spotarm_driver, waiting_nodes, initial_manipulator_positioning] + slam_and_octomap_nodes
 
 
 def generate_launch_description():
+    node_list = []
     webots = WebotsLauncher(
         world=PathJoinSubstitution([package_dir, "worlds", "spot.wbt"])
     )
+
+    node_list.append(webots)
+
     ros2_supervisor = Ros2SupervisorLauncher()
+    node_list.append(ros2_supervisor)
 
     spot_driver = WebotsController(
         robot_name="Spot",
@@ -107,6 +158,7 @@ def generate_launch_description():
         ],
         respawn=True,
     )
+    node_list.append(spot_driver)
 
     with open(os.path.join(package_dir, "resource", "spot.urdf")) as f:
         robot_desc = f.read()
@@ -123,6 +175,8 @@ def generate_launch_description():
         ],
     )
 
+    node_list.append(robot_state_publisher)
+
     # spot_pointcloud2 = Node(
     #     package='webots_spot',
     #     executable='spot_pointcloud2',
@@ -134,9 +188,14 @@ def generate_launch_description():
     reset_handler = launch.actions.RegisterEventHandler(
         event_handler=launch.event_handlers.OnProcessExit(
             target_action=ros2_supervisor,
-            on_exit=get_ros2_nodes,
+            on_exit=[
+                EmitEvent(event=Shutdown(reason='Resetting SLAM and Octomap nodes')),
+                *get_ros2_nodes()
+            ],
         )
     )
+
+    node_list.append(reset_handler)
 
     webots_event_handler = launch.actions.RegisterEventHandler(
         event_handler=launch.event_handlers.OnProcessExit(
@@ -144,6 +203,8 @@ def generate_launch_description():
             on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
         )
     )
+
+    node_list.append(webots_event_handler)
 
     pointcloud_to_laserscan_node = Node(
         package="pointcloud_to_laserscan",
@@ -168,17 +229,41 @@ def generate_launch_description():
         ],
         name="pointcloud_to_laserscan",
     )
+    node_list.append(pointcloud_to_laserscan_node)
+
+    map_1m = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        output="screen",
+        arguments=["0", "0", "1", "0", "0", "0", "map", "map_1m"],
+    )
+    node_list.append(map_1m)
+
+    map_2m = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        output="screen",
+        arguments=["0", "0", "2", "0", "0", "0", "map", "map_2m"],
+    )
+    node_list.append(map_2m)
+
+    map_odom = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        output="screen",
+        arguments=["0", "0", "0", "0", "0", "0", "map", "odom"],
+    )
+    node_list.append(map_odom)
+
+    body_base_link = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        output="screen",
+        arguments=["0", "0", "0", "0", "0", "0", "base_link", "body"],
+    )
+    node_list.append(body_base_link)
 
     return LaunchDescription(
-        [
-            webots,
-            ros2_supervisor,
-            spot_driver,
-            robot_state_publisher,
-            # spot_pointcloud2,
-            webots_event_handler,
-            reset_handler,
-            pointcloud_to_laserscan_node,
-        ]
-        + get_ros2_nodes()
+        node_list +
+        get_ros2_nodes()
     )
